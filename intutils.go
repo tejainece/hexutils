1b7a99baf3f0392f915c9ccc3bd2e29fb5430a35
Ravi Teja Gudapati <tejainece@gmail.com>
2016-07-08 18:02:33 +0200

    uint32 from uint16s

--- intutils.go
+++ intutils.go
@@ -14,3 +14,14 @@ func MakeUInt16(aBytes [2]byte) uint16 {
 
 	return lRet
 }
+
+//MakeUInt32FromUInt16 makes uint32 from two uint16
+func MakeUInt32FromUInt16(aHi, aLo uint16) uint32 {
+	lRet := uint32(0)
+
+	lRet = uint32(aHi)
+	lRet = lRet << 16
+	lRet |= uint32(aLo)
+
+	return lRet
+}
